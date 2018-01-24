2a092e42a5cea7624c543602cb1b77b9d2ee43c6
Zach Zimmerman <zzimmerman-student@nfcs.k12.pa.us>
2018-01-24 13:45:45 -0500

    Add files via upload

---
+++ PokemonCardGame/deck.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class deck here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class deck
+{
+    //pokenon array
+    String [] pokemon = {"Eevee", "Growlithe", "Cyndaquil",
+        "Natu", "Ralts", "Torchic", "Riolu", "Piplup", "Zorua",
+        "Honedge", "Bunnelby", "Rockruff","Jangmo'o"};
+    public void deck(){
+        for (i = 0)
+    }
+    
+}
---
+++ PokemonCardGame/player.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class player here.
+ *
+ * @author Zach Zimmerman
+ * @version 1.24.18
+ */
+public class player
+{
+    int health = 100;
+    //int damage = (int)(Math.random().nextInt(15, 35 + 1));
+    
+    public void attack(){
+        
+    }
+    
+    public void defend(){
+        
+    }
+}
